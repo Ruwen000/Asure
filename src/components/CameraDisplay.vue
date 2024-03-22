665a30859469a11a216f22b52ab5c9bb7fe5c46f
Mich <mally.dominik@gmx.de>
2024-03-22 15:05:07 +0100

    small cleanup

--- src/components/CameraDisplay.vue
+++ src/components/CameraDisplay.vue
@@ -1,7 +1,7 @@
 <template>
     <div class='ui container'>
         <video v-if="!imageData.image" ref="video" class="camera-stream" />
-        <img v-else :src="imageData.image" v-bind:style="{transform: 'rotate(' + imageData.image_orientation + 'deg'}" class="camera-stream">
+        <img v-else :src="imageData.image" v-bind:style="{transform: 'rotate(' + main_image_orientation + 'deg'}" class="camera-stream">
         <div class='ui divider'></div>
         <div class="icon-group">   
             <div  class="camera-icon" @click="captureImage">
@@ -11,10 +11,6 @@
             <div class='camera-icon' @click="rotateImage">
                 <i class="big redo alternate icon"></i>
                 <button>Rotate</button> 
-            </div>
-            <div  class='camera-icon' @click="uploadImage">
-                <i class="big thumbs up outline icon"></i>
-                <button>Done</button> 
             </div>
             <div class="camera-icon" @click="cancelImage">
                 <i class="big cancel icon"></i>
@@ -26,35 +22,27 @@
             <img v-for="(imageData, index) in imageList" :key="index" :src="imageData.image" class="camera-stream" />
         </div>
 
-        <!--
-            <img v-if="!imageData.image" class="camera-stream" src="../../src/assets/image.png">
-            <img v-else :src="imageData.image" class="camera-stream" />
-        -->
-
     </div>
-
 
 
 </template>
 
 <script>
-    import axios from 'axios';
-    const API_IMAGE_ENDPOINT = 'https://ApiBackend.com/api/images';
 
 export default {
     
-
     data() {
         return {
             defaultImage: '../../src/assets/image.png',
             mediaStream: null,
 
             imageList: [],     // Array to hold multiple images 
-
             imageData: {
-                image: '',
-                image_orientation: 0,
+                image: ``,
+                image_orientation:0,
             },
+
+            main_image_orientation: 0,
         }
     },
     methods: {
@@ -71,19 +59,20 @@ export default {
                 reader.onload = () => {
                     this.imageList.push({
                         image: reader.result,
-                        image_orientation: 0,
+                        image_orientation: main_image_orientation,
                     })
-                    // this.imageData.image = reader.result;
                 }
             })  
         },
 
         rotateImage() {
-            this.imageData.image_orientation = this.imageData.image_orientation + 90; 
+
+            // this.main_image_orientation = this.main_image_orientation + 90; 
+            this.main_image_orientation += 90; 
         },
 
         cancelImage() {
-            this.imageData.image = null;
+
             this.showCameraModal = true;
             navigator.mediaDevices.getUserMedia({video: true})
             .then(mediaStream => {
@@ -93,15 +82,9 @@ export default {
             }) 
         },
 
-        uploadImage() {
-            axios({ method: "POST", "url": API_IMAGE_ENDPOINT, "data": this.imageData})
-                    .then(response => {
-                        this.response = response.data;    
-                     })
-        },
-
     },
     mounted() {
+        
         navigator.mediaDevices.getUserMedia({video: true})
             .then(mediaStream => {
                     this.$refs.video.srcObject = mediaStream;
